7472cd755662829235aabfec3d20c044eca1e067
ATJP88 <93024157+ATJP88@users.noreply.github.com>
2023-11-19 13:20:20 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ gcloud config set project ${PROJECT}
 gcloud config set account gke-admin@intrepid-league-397203.iam.gserviceaccount.com
 gcloud auth activate-service-account --key-file=./service-account.json
 gcloud config list
-gcloud container clusters get-credentials k8s-cluster-dev --region europe-west2 --project intrepid-league-397203
+gcloud container clusters get-credentials hipster-dev --region europe-west2 --project intrepid-league-397203
